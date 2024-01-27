d1217e590d1a9ac0989dd12e30660c4a0dd08547
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-01-27 17:59:05 +0800

    feat: prettier code

--- mini-svelte/v2/app.js
+++ mini-svelte/v2/app.js
@@ -1,118 +1,117 @@
+export default function () {
+  let button_1;
+  let txt_2;
+  let button_3;
+  let txt_4;
+  let if_block_5;
+  let if_block_5_func;
+  let if_block_5_func_call;
+  let div_6;
+  let txt_7;
+  let ul_8;
+  let li_9;
+  let txt_10;
+  let li_11;
+  let txt_12;
+  let txt_13;
+  let txt_14;
 
-      export default function() {
-        let button_1;
-let txt_2;
-let button_3;
-let txt_4;
-let if_block_5;
-let if_block_5_func;
-let if_block_5_func_call;
-let div_6;
-let txt_7;
-let ul_8;
-let li_9;
-let txt_10;
-let li_11;
-let txt_12;
-let txt_13;
-let txt_14;
-        
-        let collectChanges = [];
-        let updateCalled = false;
-        function update(changed) {
-          changed.forEach(c => collectChanges.push(c));
+  let collectChanges = [];
+  let updateCalled = false;
+  function update(changed) {
+    changed.forEach((c) => collectChanges.push(c));
 
-          if (updateCalled) return;
-          updateCalled = true;
+    if (updateCalled) return;
+    updateCalled = true;
 
-          if (typeof lifecycle !== 'undefined') {
-            lifecycle.update(collectChanges);
+    if (typeof lifecycle !== "undefined") {
+      lifecycle.update(collectChanges);
+    }
+    collectChanges = [];
+    updateCalled = false;
+  }
+  let count = 0;
+  const addCount = () => {
+    count++, update(["count"]);
+  };
+  const subCount = () => {
+    count--, update(["count"]);
+  };
+
+  var lifecycle = {
+    create(target) {
+      button_1 = document.createElement("button");
+      button_1.addEventListener("click", addCount);
+      txt_2 = document.createTextNode("count++");
+      button_1.appendChild(txt_2);
+      target.appendChild(button_1);
+      button_3 = document.createElement("button");
+      button_3.addEventListener("click", subCount);
+      txt_4 = document.createTextNode("count--");
+      button_3.appendChild(txt_4);
+      target.appendChild(button_3);
+      if_block_5_func = () => {
+        if (count > 0 && count < 5) {
+          if (if_block_5_func_call) {
+            return;
           }
-          collectChanges = [];
-          updateCalled = false;
-        }
-        let count = 0;
-const addCount = () => {
-    count++, update(['count']);
-};
-const subCount = () => {
-    count--, update(['count']);
-};
 
-        var lifecycle = {
-          create(target) {
-            button_1 = document.createElement('button')
-button_1.addEventListener('click', addCount);
-txt_2 = document.createTextNode('count++');
-button_1.appendChild(txt_2)
-target.appendChild(button_1)
-button_3 = document.createElement('button')
-button_3.addEventListener('click', subCount);
-txt_4 = document.createTextNode('count--');
-button_3.appendChild(txt_4)
-target.appendChild(button_3)
-if_block_5_func = () => {
-if (count > 0 && count < 5) {
-          if (if_block_5_func_call) {return;}
-        
+          if_block_5 = document.createElement("span");
 
-          if_block_5 = document.createElement('span');
-        
-div_6 = document.createElement('div')
-txt_7 = document.createTextNode('显示');
-div_6.appendChild(txt_7)
-if_block_5.appendChild(div_6)
-target.appendChild(if_block_5)
+          div_6 = document.createElement("div");
+          txt_7 = document.createTextNode("显示");
+          div_6.appendChild(txt_7);
+          if_block_5.appendChild(div_6);
+          target.appendChild(if_block_5);
 
           if_block_5_func_call = true;
         } else {
           if_block_5_func_call = false;
-        
-console.log('zzh parentnode', if_block_5);
-if (if_block_5 && if_block_5.parentNode) {
+
+          if (if_block_5 && if_block_5.parentNode) {
             if_block_5.parentNode.removeChild(if_block_5);
           }
-}}
-if_block_5_func()
-ul_8 = document.createElement('ul')
-li_9 = document.createElement('li')
-txt_10 = document.createTextNode('one');
-li_9.appendChild(txt_10)
-ul_8.appendChild(li_9)
-li_11 = document.createElement('li')
-txt_12 = document.createTextNode('two');
-li_11.appendChild(txt_12)
-ul_8.appendChild(li_11)
-target.appendChild(ul_8)
-txt_13 = document.createTextNode('count:');
-target.appendChild(txt_13)
-txt_14 = document.createTextNode(count)
-target.appendChild(txt_14);
-          },
-          update(changed) {
-            if_block_5_func()
-if (changed.includes('count')) {
-            txt_14.data = count;
-          }
-          },
-          destroy(target) {
-            button_1.removeEventListener('click', 'addCount');
-button_1.removeChild(txt_2)
-target.removeChild(button_1)
-button_3.removeEventListener('click', 'subCount');
-button_3.removeChild(txt_4)
-target.removeChild(button_3)
-div_6.removeChild(txt_7)
-if_block_5.removeChild(div_6)
-target.removeChild(if_block_5)
-li_9.removeChild(txt_10)
-ul_8.removeChild(li_9)
-li_11.removeChild(txt_12)
-ul_8.removeChild(li_11)
-target.removeChild(ul_8)
-target.removeChild(txt_13)
-          }
-        };
-        return lifecycle;
+        }
+      };
+      if_block_5_func();
+      ul_8 = document.createElement("ul");
+      li_9 = document.createElement("li");
+      txt_10 = document.createTextNode("one");
+      li_9.appendChild(txt_10);
+      ul_8.appendChild(li_9);
+      li_11 = document.createElement("li");
+      txt_12 = document.createTextNode("two");
+      li_11.appendChild(txt_12);
+      ul_8.appendChild(li_11);
+      target.appendChild(ul_8);
+      txt_13 = document.createTextNode("count:");
+      target.appendChild(txt_13);
+      txt_14 = document.createTextNode(count);
+      target.appendChild(txt_14);
+    },
+    update(changed) {
+      if_block_5_func();
+      if (changed.includes("count")) {
+        txt_14.data = count;
       }
-    
+    },
+    destroy(target) {
+      button_1.removeEventListener("click", "addCount");
+      button_1.removeChild(txt_2);
+      target.removeChild(button_1);
+      button_3.removeEventListener("click", "subCount");
+      button_3.removeChild(txt_4);
+      target.removeChild(button_3);
+      div_6.removeChild(txt_7);
+      if_block_5.removeChild(div_6);
+      target.removeChild(if_block_5);
+      li_9.removeChild(txt_10);
+      ul_8.removeChild(li_9);
+      li_11.removeChild(txt_12);
+      ul_8.removeChild(li_11);
+      target.removeChild(ul_8);
+      target.removeChild(txt_13);
+    },
+  };
+  return lifecycle;
+}
---
+++ mini-svelte/v2/prettier.js
@@ -0,0 +1,457 @@
+import * as fs from "fs";
+import * as acorn from "acorn";
+import * as periscopic from "periscopic"; // todo 作用？
+import * as estreewalker from "estree-walker"; // todo 作用？
+import * as escodegen from "escodegen"; // todo 作用？
+import * as prettier from "prettier";
+
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+
+const modulePath = dirname(fileURLToPath(import.meta.url));
+
+// 将svelte文件转成浏览器可以执行的js文件
+async function buildAppJs() {
+  try {
+    const inputPath = resolve(modulePath, "./app-10.svelte")
+    const outputPath = resolve(modulePath, "./app.js")
+    const content = fs.readFileSync(inputPath, "utf-8");
+    fs.writeFileSync(outputPath, compile(content), "utf-8");
+    const compiledContent = fs.readFileSync(outputPath, "utf-8");
+    const prettierContent = await prettier.format(compiledContent, {parser: 'babel'});
+    fs.writeFileSync(outputPath, prettierContent, "utf-8");
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function compile(content) {
+  console.log('compile');
+  const ast = parse(content); // 解析svelte文件内容成ast
+  const analysis = analyse(ast); // 解析有哪些会更新的变量
+  return generate(ast , analysis);
+}
+
+function parse(content) {
+  console.log('parse')
+  let i = 0;
+  const ast = {};
+  ast.html = parseFragments(() => i < content.length);
+  return ast;
+
+  function parseFragments(condition) {
+    const fragments = [];
+    // const fragment = parseFragment();
+    while (condition()) {
+      const fragment = parseFragment();
+      if (fragment) {
+        fragments.push(fragment);
+      }
+    }
+    return fragments;
+  }
+
+  function parseFragment() {
+    return parseScript() ?? parseElement() ?? parseExpression() ?? parseText();
+  }
+
+  function parseScript() {
+    console.log('parseScript');
+    skipWhitespace();
+    if (match("<script>")) {
+      eat("<script>");
+      const startIndex = i;
+      const endIndex = content.indexOf("</script>", i);
+      const code = content.slice(startIndex, endIndex);
+      ast.script = acorn.parse(code, { ecmaVersion: 2023 });
+      i = endIndex;
+      eat("</script>");
+      skipWhitespace();
+    }
+  }
+
+  function parseElement() {
+    skipWhitespace();
+    if (match('<')) {
+      eat('<');
+      const tagName = readWhileMatching(/[a-z]/);
+      const attributes = parseAttributeList();
+      eat('>');
+      const endTag = `</${tagName}>`;
+      const element = {
+        type: 'Element',
+        name: tagName,
+        attributes,
+        children: parseFragments(() => !match(endTag)),
+      };
+      eat(endTag);
+      skipWhitespace();
+      return element;
+    }
+  }
+
+  function parseAttributeList() {
+    skipWhitespace();
+    const attributes = [];
+    while(!match('>')) {
+      attributes.push(parseAttribute());
+      skipWhitespace();
+    }
+    return attributes;
+  }
+
+  function parseAttribute() {
+    console.log('parseAttribute');
+    const name = readWhileMatching(/[^=]/);
+    if (match('={')) {
+      eat('={');
+      const value = parseJavaScript();
+      eat('}');
+      return {
+        type: 'Attribute',
+        name,
+        value,
+      };
+    }
+  }
+
+  function parseExpression() {
+    console.log('parseExpression')
+    if (match('{') && !match('{#')) {
+      eat('{');
+      const expression = parseJavaScript();
+      eat('}');
+      return {
+        type: 'Expression',
+        expression,
+      };
+    } else if (match('{#')) {
+      return parseBlock();
+    }
+  }
+
+  function parseBlock() {
+    console.log('parseBlock');
+    if (match('{#if')) {
+      eat('{#if')
+      skipWhitespace();
+      const expression = parseJavaScript();
+      eat('}');
+      const endTag = '{/if}';
+      const block = {
+        type: 'IfBlock',
+        expression,
+        children: parseFragments(() => !match(endTag))
+      }
+      eat(endTag);
+      return block;
+    }
+  }
+
+  function parseJavaScript() {
+    const js = acorn.parseExpressionAt(content, i, { ecmaVersion: 2023 });
+    i = js.end;
+    return js;
+  }
+
+  function parseText() {
+    console.log('parseText');
+    const text = readWhileMatching(/[^<{]/);
+    if (text.trim() !== '') {
+      return {
+        type: 'Text',
+        value: text,
+      }
+    }
+  }
+
+  function match(str) {
+    return content.slice(i, i + str.length) === str;
+  }
+
+  function eat(str) {
+    if (match(str)) {
+      i += str.length;
+    } else {
+      throw new Error(`Parse error: expecting "${str}"`);
+    }
+  }
+
+  function readWhileMatching(reg) {
+    let startIndex = i;
+    while(i < content.length && reg.test(content[i])) {
+      i++;
+    }
+    return content.slice(startIndex, i);
+  }
+
+  function skipWhitespace() {
+    readWhileMatching(/[\s\n]/);
+  }
+}
+
+function analyse(ast) {
+  const result = {
+    variables: new Set(),
+    willChange: new Set(),
+    willUseInTemplate: new Set(),
+  };
+
+  const { scope: rootScope, map, globals } = periscopic.analyze(ast.script);
+  result.variables = new Set(rootScope.declarations.keys());
+  result.rootScope = rootScope;
+  result.map = map;
+
+  let currentScope = rootScope;
+  estreewalker.walk(ast.script, {
+    enter(node) {
+      if (map.has(node)) {
+        currentScope = map.get(node)
+      }
+      if (node.type === 'UpdateExpression' || node.type === 'AssignmentExpression') {
+        const names = periscopic.extract_names(node.type === 'UpdateExpression' ? node.argument : node.left);
+        for(const name of names) {
+          if (currentScope.find_owner(name) === rootScope || globals.has(name)) {
+            result.willChange.add(name);
+          }
+        }
+      }
+    },
+    leave(node) {
+      if (map.has(node)) {
+        currentScope = currentScope.parent;
+      }
+    }
+  })
+
+  function traverse(fragment) {
+    switch(fragment.type) {
+      case 'Element':
+        fragment.children.forEach((child) => traverse(child));
+        fragment.attributes.forEach((attribute) => traverse(attribute));
+        break;
+      case 'Attribute':
+        result.willUseInTemplate.add(fragment.value.name);
+        break;
+      case 'Expression': {
+        extract_names(fragment.expression).forEach((name) => {
+          result.willUseInTemplate.add(name);
+        });
+        break;
+      } 
+    }
+  }
+  ast.html.forEach(fragment => traverse(fragment));
+  return result;
+}
+
+function extract_names(jsNode, result = []) {
+  switch (jsNode.type) {
+    case 'Identifier':
+      result.push(jsNode.name);
+      break;
+    case 'BinaryExpression':
+      extract_names(jsNode.left, result);
+      extract_names(jsNode.right, result);
+      break;
+  }
+  return result;
+}
+
+function generate(ast, analysis) {
+  const code = {
+    variables: [],
+    create: [],
+    update: [],
+    destroy: [],
+  };
+
+  let counter = 1;
+
+  function traverse(node, parent) {
+    switch(node.type) {
+      case 'Element': {
+        const variableName = `${node.name}_${counter++}`;
+        code.variables.push(variableName);
+        code.create.push(
+          `${variableName} = document.createElement('${node.name}')`
+        )
+        node.attributes.forEach(attribute => {
+          traverse(attribute, variableName);
+        })
+
+        node.children.forEach(child => {
+          traverse(child, variableName);
+        })
+
+        code.create.push(`${parent}.appendChild(${variableName})`);
+        code.destroy.push(`${parent}.removeChild(${variableName})`);
+        break;
+      }
+      case 'Attribute': {
+        if (node.name.startsWith('on:')) {
+          const eventName = node.name.slice(3);
+          const eventHandler = node.value.name;
+          code.create.push(`${parent}.addEventListener('${eventName}', ${eventHandler});`);
+          code.destroy.push(`${parent}.removeEventListener('${eventName}', '${eventHandler}');`);
+        }
+        break;
+      }
+      case 'Text': {
+        const variableName = `txt_${counter++}`;
+        code.variables.push(variableName);
+        code.create.push(`${variableName} = document.createTextNode('${node.value}');`);
+        code.create.push(`${parent}.appendChild(${variableName})`);
+        code.destroy.push(`${parent}.removeChild(${variableName})`);
+        break;
+      }
+      case 'Expression': {
+        const variableName = `txt_${counter++}`;
+        const expressionStr = escodegen.generate(node.expression);
+        code.variables.push(variableName);
+        code.create.push(
+          `${variableName} = document.createTextNode(${expressionStr})`
+        );
+        code.create.push(`${parent}.appendChild(${variableName});`);
+
+        // 更新
+        const names = extract_names(node.expression);// todo extract_names
+        if (names.some((name) => analysis.willChange.has(name))) {
+          const changes = new Set();
+          names.forEach(name => {
+            if (analysis.willChange.has(name)) {
+              changes.add(name);
+            }
+          });
+          let condition;
+          if (changes.size > 1) {
+            condition = `${JSON.stringify(
+              Array.from(changes))}.some(name => changed.includes(name))`;
+          } else {
+            condition = `changed.includes('${Array.from(changes)[0]}')`
+          }
+          code.update.push(`if (${condition}) {
+            ${variableName}.data = ${expressionStr};
+          }`);
+        }
+        break;
+      }
+      case 'IfBlock': {
+        const variableName = `if_block_${counter++}`;
+        const funcName = `${variableName}_func`;
+        const funcCallName = `${funcName}_call`;
+        const expressionStr = escodegen.generate(node.expression);
+        code.variables.push(variableName);
+        code.variables.push(funcName);
+        code.variables.push(funcCallName);
+        code.create.push(`${funcName} = () => {`)
+        
+        code.create.push(`if (${expressionStr}) {
+          if (${funcCallName}) {return;}
+        `);
+        code.create.push(`
+          ${variableName} = document.createElement('span');
+        `);
+        node.children.forEach(subNode => {
+          traverse(subNode, variableName)
+        })
+        code.create.push(`${parent}.appendChild(${variableName})`);
+        
+        code.destroy.push(`${parent}.removeChild(${variableName})`);
+          
+        code.create.push(`
+          ${funcCallName} = true;
+        } else {
+          ${funcCallName} = false;
+        `);
+        code.create.push(`if (${variableName} && ${variableName}.parentNode) {
+          ${variableName}.parentNode.removeChild(${variableName});
+        }`);
+        code.create.push('}}');
+
+        code.create.push(`${funcName}()`);// call function
+        code.update.push(`${funcName}()`);
+        
+        break;
+      }
+    }
+  }
+
+  ast.html.forEach((fragment) => traverse(fragment, 'target'));
+
+  const { rootScope, map } = analysis;
+  let currentScope = rootScope;
+  estreewalker.walk(ast.script, {
+    enter(node, parent) {
+      if (map.has(node)) {
+        currentScope = map.get(node)
+      }
+      if (node.type === 'UpdateExpression' || node.type === 'AssignmentExpression') {
+        const names = periscopic
+          .extract_names(
+            node.type === 'UpdateExpression' ? node.argument : node.left
+          )
+          .filter(
+            (name) =>
+              currentScope.find_owner(name) === rootScope &&
+              analysis.willUseInTemplate.has(name)
+          );
+        if (names.length > 0) {
+          this.replace({ // todo
+            type: 'SequenceExpression',
+            expressions: [
+              node,
+              acorn.parseExpressionAt(`update(${JSON.stringify(names)})`, 0, {
+                ecmaVersion: 2023,
+              }),
+            ],
+          });
+          this.skip(); // todo
+        }
+      }
+    },
+    leave(node) {
+      if (map.has(node)) {
+        currentScope = currentScope.parent;
+      }
+    }
+  })
+
+  return `
+      export default function() {
+        ${code.variables.map(v => `let ${v};`).join('\n')}
+        
+        let collectChanges = [];
+        let updateCalled = false;
+        function update(changed) {
+          changed.forEach(c => collectChanges.push(c));
+
+          if (updateCalled) return;
+          updateCalled = true;
+
+          if (typeof lifecycle !== 'undefined') {
+            lifecycle.update(collectChanges);
+          }
+          collectChanges = [];
+          updateCalled = false;
+        }
+        ${escodegen.generate(ast.script)}
+
+        var lifecycle = {
+          create(target) {
+            ${code.create.join('\n')}
+          },
+          update(changed) {
+            ${code.update.join('\n')}
+          },
+          destroy(target) {
+            ${code.destroy.join('\n')}
+          }
+        };
+        return lifecycle;
+      }
+    `;
+}
+
+buildAppJs();
