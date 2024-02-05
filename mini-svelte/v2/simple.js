ecf355c097fd82c5dad42a8fa6201693d95745f2
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-02-05 15:16:30 +0800

    feat: lint

--- mini-svelte/v2/simple.js
+++ mini-svelte/v2/simple.js
@@ -5,21 +5,22 @@ import * as estreewalker from "estree-walker"; // todo 作用？
 import * as escodegen from "escodegen"; // todo 作用？
 import * as prettier from "prettier";
 
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 const modulePath = dirname(fileURLToPath(import.meta.url));
 
 // 将svelte文件转成浏览器可以执行的js文件
 async function buildAppJs() {
   try {
-    const inputPath = resolve(modulePath, "./app-10.svelte")
-    const outputPath = resolve(modulePath, "./app.js")
+    const inputPath = resolve(modulePath, "./app-10.svelte");
+    const outputPath = resolve(modulePath, "./app.js");
     const content = fs.readFileSync(inputPath, "utf-8");
     fs.writeFileSync(outputPath, compile(content), "utf-8");
     const compiledContent = fs.readFileSync(outputPath, "utf-8");
-    const prettierContent = await prettier.format(compiledContent, {parser: 'babel'});
+    const prettierContent = await prettier.format(compiledContent, {
+      parser: "babel",
+    });
     fs.writeFileSync(outputPath, prettierContent, "utf-8");
   } catch (e) {
     console.error(e);
@@ -27,14 +28,14 @@ async function buildAppJs() {
 }
 
 function compile(content) {
-  console.log('compile');
+  console.log("compile");
   const ast = parse(content); // 解析svelte文件内容成ast
   const analysis = analyse(ast); // 解析有哪些会更新的变量
-  return generate(ast , analysis);
+  return generate(ast, analysis);
 }
 
 function parse(content) {
-  console.log('parse')
+  console.log("parse");
   let i = 0;
   const ast = {};
   ast.html = parseFragments(() => i < content.length);
@@ -57,7 +58,7 @@ function parse(content) {
   }
 
   function parseScript() {
-    console.log('parseScript');
+    console.log("parseScript");
     skipWhitespace();
     if (match("<script>")) {
       eat("<script>");
@@ -73,14 +74,14 @@ function parse(content) {
 
   function parseElement() {
     skipWhitespace();
-    if (match('<')) {
-      eat('<');
+    if (match("<")) {
+      eat("<");
       const tagName = readWhileMatching(/[a-z]/);
       const attributes = parseAttributeList();
-      eat('>');
+      eat(">");
       const endTag = `</${tagName}>`;
       const element = {
-        type: 'Element',
+        type: "Element",
         name: tagName,
         attributes,
         children: parseFragments(() => !match(endTag)),
@@ -94,7 +95,7 @@ function parse(content) {
   function parseAttributeList() {
     skipWhitespace();
     const attributes = [];
-    while(!match('>')) {
+    while (!match(">")) {
       attributes.push(parseAttribute());
       skipWhitespace();
     }
@@ -102,14 +103,14 @@ function parse(content) {
   }
 
   function parseAttribute() {
-    console.log('parseAttribute');
+    console.log("parseAttribute");
     const name = readWhileMatching(/[^=]/);
-    if (match('={')) {
-      eat('={');
+    if (match("={")) {
+      eat("={");
       const value = parseJavaScript();
-      eat('}');
+      eat("}");
       return {
-        type: 'Attribute',
+        type: "Attribute",
         name,
         value,
       };
@@ -117,33 +118,33 @@ function parse(content) {
   }
 
   function parseExpression() {
-    console.log('parseExpression')
-    if (match('{') && !match('{#')) {
-      eat('{');
+    console.log("parseExpression");
+    if (match("{") && !match("{#")) {
+      eat("{");
       const expression = parseJavaScript();
-      eat('}');
+      eat("}");
       return {
-        type: 'Expression',
+        type: "Expression",
         expression,
       };
-    } else if (match('{#')) {
+    } else if (match("{#")) {
       return parseBlock();
     }
   }
 
   function parseBlock() {
-    console.log('parseBlock');
-    if (match('{#if')) {
-      eat('{#if')
+    console.log("parseBlock");
+    if (match("{#if")) {
+      eat("{#if");
       skipWhitespace();
       const expression = parseJavaScript();
-      eat('}');
-      const endTag = '{/if}';
+      eat("}");
+      const endTag = "{/if}";
       const block = {
-        type: 'IfBlock',
+        type: "IfBlock",
         expression,
-        children: parseFragments(() => !match(endTag))
-      }
+        children: parseFragments(() => !match(endTag)),
+      };
       eat(endTag);
       return block;
     }
@@ -156,13 +157,13 @@ function parse(content) {
   }
 
   function parseText() {
-    console.log('parseText');
+    console.log("parseText");
     const text = readWhileMatching(/[^<{]/);
-    if (text.trim() !== '') {
+    if (text.trim() !== "") {
       return {
-        type: 'Text',
+        type: "Text",
         value: text,
-      }
+      };
     }
   }
 
@@ -180,7 +181,7 @@ function parse(content) {
 
   function readWhileMatching(reg) {
     let startIndex = i;
-    while(i < content.length && reg.test(content[i])) {
+    while (i < content.length && reg.test(content[i])) {
       i++;
     }
     return content.slice(startIndex, i);
@@ -207,12 +208,20 @@ function analyse(ast) {
   estreewalker.walk(ast.script, {
     enter(node) {
       if (map.has(node)) {
-        currentScope = map.get(node)
+        currentScope = map.get(node);
       }
-      if (node.type === 'UpdateExpression' || node.type === 'AssignmentExpression') {
-        const names = periscopic.extract_names(node.type === 'UpdateExpression' ? node.argument : node.left);
-        for(const name of names) {
-          if (currentScope.find_owner(name) === rootScope || globals.has(name)) {
+      if (
+        node.type === "UpdateExpression" ||
+        node.type === "AssignmentExpression"
+      ) {
+        const names = periscopic.extract_names(
+          node.type === "UpdateExpression" ? node.argument : node.left
+        );
+        for (const name of names) {
+          if (
+            currentScope.find_owner(name) === rootScope ||
+            globals.has(name)
+          ) {
             result.willChange.add(name);
           }
         }
@@ -222,36 +231,36 @@ function analyse(ast) {
       if (map.has(node)) {
         currentScope = currentScope.parent;
       }
-    }
-  })
+    },
+  });
 
   function traverse(fragment) {
-    switch(fragment.type) {
-      case 'Element':
+    switch (fragment.type) {
+      case "Element":
         fragment.children.forEach((child) => traverse(child));
         fragment.attributes.forEach((attribute) => traverse(attribute));
         break;
-      case 'Attribute':
+      case "Attribute":
         result.willUseInTemplate.add(fragment.value.name);
         break;
-      case 'Expression': {
+      case "Expression": {
         extract_names(fragment.expression).forEach((name) => {
           result.willUseInTemplate.add(name);
         });
         break;
-      } 
+      }
     }
   }
-  ast.html.forEach(fragment => traverse(fragment));
+  ast.html.forEach((fragment) => traverse(fragment));
   return result;
 }
 
 function extract_names(jsNode, result = []) {
   switch (jsNode.type) {
-    case 'Identifier':
+    case "Identifier":
       result.push(jsNode.name);
       break;
-    case 'BinaryExpression':
+    case "BinaryExpression":
       extract_names(jsNode.left, result);
       extract_names(jsNode.right, result);
       break;
@@ -270,37 +279,37 @@ function generate(ast, analysis) {
   let counter = 1;
 
   function traverse(node, parent) {
-    switch(node.type) {
-      case 'Element': {
+    switch (node.type) {
+      case "Element": {
         const variableName = `${node.name}_${counter++}`;
         code.variables.push(variableName);
-        code.create.push(
-          `${variableName} = element('${node.name}')`
-        )
-        node.attributes.forEach(attribute => {
+        code.create.push(`${variableName} = element('${node.name}')`);
+        node.attributes.forEach((attribute) => {
           traverse(attribute, variableName);
-        })
+        });
 
-        node.children.forEach(child => {
+        node.children.forEach((child) => {
           traverse(child, variableName);
-        })
+        });
 
         code.create.push(`append(${parent}, ${variableName})`);
         code.destroy.push(`detach(${variableName})`);
         break;
       }
-      case 'Attribute': {
-        if (node.name.startsWith('on:')) {
+      case "Attribute": {
+        if (node.name.startsWith("on:")) {
           const eventName = node.name.slice(3);
           const eventHandler = node.value.name;
           const eventNameCall = `${eventName}_${counter++}`;
           code.variables.push(eventNameCall);
-          code.create.push(`${eventNameCall} = listen(${parent}, "${eventName}", ${eventHandler})`)
+          code.create.push(
+            `${eventNameCall} = listen(${parent}, "${eventName}", ${eventHandler})`
+          );
           code.destroy.push(`${eventNameCall}()`);
         }
         break;
       }
-      case 'Text': {
+      case "Text": {
         const variableName = `txt_${counter++}`;
         code.variables.push(variableName);
         code.create.push(`${variableName} = text('${node.value}');`);
@@ -308,20 +317,18 @@ function generate(ast, analysis) {
         code.destroy.push(`detach(${variableName})`);
         break;
       }
-      case 'Expression': {
+      case "Expression": {
         const variableName = `txt_${counter++}`;
         const expressionStr = escodegen.generate(node.expression);
         code.variables.push(variableName);
-        code.create.push(
-          `${variableName} = text(${expressionStr})`
-        );
+        code.create.push(`${variableName} = text(${expressionStr})`);
         code.create.push(`append(${parent}, ${variableName});`);
 
         // 更新
-        const names = extract_names(node.expression);// todo extract_names
+        const names = extract_names(node.expression); // todo extract_names
         if (names.some((name) => analysis.willChange.has(name))) {
           const changes = new Set();
-          names.forEach(name => {
+          names.forEach((name) => {
             if (analysis.willChange.has(name)) {
               changes.add(name);
             }
@@ -329,9 +336,10 @@ function generate(ast, analysis) {
           let condition;
           if (changes.size > 1) {
             condition = `${JSON.stringify(
-              Array.from(changes))}.some(name => changed.includes(name))`;
+              Array.from(changes)
+            )}.some(name => changed.includes(name))`;
           } else {
-            condition = `changed.includes('${Array.from(changes)[0]}')`
+            condition = `changed.includes('${Array.from(changes)[0]}')`;
           }
           code.update.push(`if (${condition}) {
             ${variableName}.data = ${expressionStr};
@@ -339,7 +347,7 @@ function generate(ast, analysis) {
         }
         break;
       }
-      case 'IfBlock': {
+      case "IfBlock": {
         const variableName = `if_block_${counter++}`;
         const funcName = `${variableName}_func`;
         const funcCallName = `${funcName}_call`;
@@ -347,50 +355,53 @@ function generate(ast, analysis) {
         code.variables.push(variableName);
         code.variables.push(funcName);
         code.variables.push(funcCallName);
-        code.create.push(`${funcName} = () => {`)
-        
+        code.create.push(`${funcName} = () => {`);
+
         code.create.push(`if (${expressionStr}) {
           if (${funcCallName}) {return;}
         `);
         code.create.push(`
           ${variableName} = element('span');
         `);
-        node.children.forEach(subNode => {
-          traverse(subNode, variableName)
-        })
+        node.children.forEach((subNode) => {
+          traverse(subNode, variableName);
+        });
         code.create.push(`append(${parent}, ${variableName})`);
-        
+
         code.destroy.push(`detach(${variableName})`);
-          
+
         code.create.push(`
           ${funcCallName} = true;
         } else {
           ${funcCallName} = false;
         `);
         code.create.push(`if (${variableName}) {detach(${variableName})}`);
-        code.create.push('}}');
+        code.create.push("}}");
 
-        code.create.push(`${funcName}()`);// call function
+        code.create.push(`${funcName}()`); // call function
         code.update.push(`${funcName}()`);
-        
+
         break;
       }
     }
   }
 
-  ast.html.forEach((fragment) => traverse(fragment, 'target'));
+  ast.html.forEach((fragment) => traverse(fragment, "target"));
 
   const { rootScope, map } = analysis;
   let currentScope = rootScope;
   estreewalker.walk(ast.script, {
     enter(node, parent) {
       if (map.has(node)) {
-        currentScope = map.get(node)
+        currentScope = map.get(node);
       }
-      if (node.type === 'UpdateExpression' || node.type === 'AssignmentExpression') {
+      if (
+        node.type === "UpdateExpression" ||
+        node.type === "AssignmentExpression"
+      ) {
         const names = periscopic
           .extract_names(
-            node.type === 'UpdateExpression' ? node.argument : node.left
+            node.type === "UpdateExpression" ? node.argument : node.left
           )
           .filter(
             (name) =>
@@ -398,8 +409,9 @@ function generate(ast, analysis) {
               analysis.willUseInTemplate.has(name)
           );
         if (names.length > 0) {
-          this.replace({ // todo
-            type: 'SequenceExpression',
+          this.replace({
+            // todo
+            type: "SequenceExpression",
             expressions: [
               node,
               acorn.parseExpressionAt(`update(${JSON.stringify(names)})`, 0, {
@@ -415,8 +427,8 @@ function generate(ast, analysis) {
       if (map.has(node)) {
         currentScope = currentScope.parent;
       }
-    }
-  })
+    },
+  });
 
   return `
       function element(name) {
@@ -437,17 +449,13 @@ function generate(ast, analysis) {
         }
       }
 
-      function empty() {
-        return text('');
-      }
-
       export function listen(node, event, handler) {
         node.addEventListener(event, handler);
         return () => node.removeEventListener(event, handler);
       }
 
       export default function() {
-        ${code.variables.map(v => `let ${v};`).join('\n')}
+        ${code.variables.map((v) => `let ${v};`).join("\n")}
         
         let collectChanges = [];
         let updateCalled = false;
@@ -467,13 +475,13 @@ function generate(ast, analysis) {
 
         var lifecycle = {
           create(target) {
-            ${code.create.join('\n')}
+            ${code.create.join("\n")}
           },
           update(changed) {
-            ${code.update.join('\n')}
+            ${code.update.join("\n")}
           },
           destroy(target) {
-            ${code.destroy.join('\n')}
+            ${code.destroy.join("\n")}
           }
         };
         return lifecycle;
